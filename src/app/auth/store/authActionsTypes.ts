63417e2055653d80fe46d1254f7b030ee9513cc8
mrnicky28 <lavrinovich_nikita@mail.ru>
2022-01-25 18:05:10 +0300

    add actionsTypes

---
+++ src/app/auth/store/authActionsTypes.ts
@@ -0,0 +1,9 @@
+export enum AuthActionTypes {
+    REGISTER = '[Auth] Register',
+    REGISTER_SUCCESS = '[Auth] Register success',
+    REGISTER_FAILURE = '[Auth] Register failure',
+
+    LOGIN = '[Auth] Login',
+    LOGIN_SUCCESS = '[Auth] Auth success',
+    LOGIN_FAILURE = '[Auth] Auth failure',
+}
