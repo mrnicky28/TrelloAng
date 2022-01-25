6d1210d27cecfd65fb20c462232e8d0584ce8d1a
mrnicky28 <lavrinovich_nikita@mail.ru>
2022-01-25 10:48:13 +0300

    add firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'trello-2f52b',
+    appId: '1:786811175501:web:43807b270d03bee9c71204',
+    storageBucket: 'trello-2f52b.appspot.com',
+    apiKey: 'AIzaSyAujMkCr_Pd-feuAqVoE1e1H0VirJyIS8Y',
+    authDomain: 'trello-2f52b.firebaseapp.com',
+    messagingSenderId: '786811175501',
+  },
   production: true
 };
