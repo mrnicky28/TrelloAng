a017412c2f675fc14fbe0b21dab465ecce3278b4
mrnicky28 <lavrinovich_nikita@mail.ru>
2022-01-25 17:58:32 +0300

    add auth module

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,36 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+
+import { MaterialModule } from '../shared/modules/material/material.module';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { reducer } from './store/authReducers';
+import { RegisterEffect } from './store/effects/register.effect';
+
+const routes: Routes = [
+    {
+        path: 'login',
+        component: LoginComponent,
+    },
+    {
+        path: 'register',
+        component: RegisterComponent,
+    },
+];
+
+@NgModule({
+    declarations: [LoginComponent, RegisterComponent],
+    imports: [
+        CommonModule,
+        RouterModule.forChild(routes),
+        StoreModule.forFeature('auth', reducer),
+        EffectsModule.forFeature([RegisterEffect]),
+        MaterialModule,
+        ReactiveFormsModule,
+    ],
+})
+export class AuthModule {}
