ddddcef83633f709e1c1969bb90648f5f1b932d2
mrnicky28 <lavrinovich_nikita@mail.ru>
2022-01-26 10:57:31 +0300

    add dashboard component

--- src/app/dasboard/components/dashboard/dashboard.component.ts
+++ src/app/dasboard/components/dashboard/dashboard.component.ts
@@ -1,15 +1,39 @@
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+    selector: 'app-dashboard',
+    templateUrl: './dashboard.component.html',
+    styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
+    ngOnInit(): void {
+        throw new Error('Method not implemented.');
+    }
+    todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
 
-  constructor() { }
+    done = [
+        'Get up',
+        'Brush teeth',
+        'Take a shower',
+        'Check e-mail',
+        'Walk dog',
+    ];
 
-  ngOnInit(): void {
-  }
-
+    drop(event: CdkDragDrop<string[]>) {
+        if (event.previousContainer === event.container) {
+            moveItemInArray(
+                event.container.data,
+                event.previousIndex,
+                event.currentIndex,
+            );
+        } else {
+            transferArrayItem(
+                event.previousContainer.data,
+                event.container.data,
+                event.previousIndex,
+                event.currentIndex,
+            );
+        }
+    }
 }
--- src/app/dasboard/dashboard.module.ts
+++ src/app/dasboard/dashboard.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { MaterialModule } from '../shared/modules/material/material.module';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
     declarations: [DashboardComponent],
-    imports: [CommonModule],
+    imports: [CommonModule, MaterialModule],
     exports: [DashboardComponent],
 })
 export class DashboardModule {}
