7f313ad8b360bec0f98e09b337550f6f9c32b034
mrnicky28 <lavrinovich_nikita@mail.ru>
2022-01-27 17:17:23 +0300

    add AddTodoDialogModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
 import { DashboardModule } from './dasboard/dashboard.module';
+import { AddTodoDialogModule } from './shared/modules/add-todo-dialog/add-todo-dialog.module';
 import { ToolbarModule } from './shared/modules/toolbar/toolbar.module';
 
 @NgModule({
@@ -35,6 +36,7 @@ import { ToolbarModule } from './shared/modules/toolbar/toolbar.module';
         AuthModule,
         ToolbarModule,
         DashboardModule,
+        AddTodoDialogModule,
     ],
     providers: [],
     bootstrap: [AppComponent],
