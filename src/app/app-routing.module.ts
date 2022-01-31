a9ae39652d19d85b2539f0766a5321fd1860f2da
mrnicky28 <lavrinovich_nikita@mail.ru>
2022-01-31 20:49:44 +0300

    add AuthGuard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { DashboardComponent } from './dasboard/components/dashboard/dashboard.component';
+import { AuthGuard } from './shared/guards/auth.guard';
 
 const routes: Routes = [
     {
         path: 'dashboard',
         component: DashboardComponent,
+        canActivate: [AuthGuard],
     },
 ];
 
