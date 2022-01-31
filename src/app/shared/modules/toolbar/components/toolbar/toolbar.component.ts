b1373d865841bb407e88dbd46ec095c1befc50ee
mrnicky28 <lavrinovich_nikita@mail.ru>
2022-01-31 20:52:23 +0300

    fixed toolbar

--- src/app/shared/modules/toolbar/components/toolbar/toolbar.component.ts
+++ src/app/shared/modules/toolbar/components/toolbar/toolbar.component.ts
@@ -1,15 +1,24 @@
+import { Observable } from 'rxjs';
+import { isLoggedInSelector } from 'src/app/auth/store/authSelectors';
+
 import { Component, OnInit } from '@angular/core';
+import { select, Store } from '@ngrx/store';
 
 @Component({
-  selector: 'app-toolbar',
-  templateUrl: './toolbar.component.html',
-  styleUrls: ['./toolbar.component.scss']
+    selector: 'app-toolbar',
+    templateUrl: './toolbar.component.html',
+    styleUrls: ['./toolbar.component.scss'],
 })
 export class ToolbarComponent implements OnInit {
+    isLoggedIn$!: Observable<boolean | null>;
 
-  constructor() { }
+    constructor(private store: Store) {}
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        this.inizializeValue();
+    }
 
+    inizializeValue(): void {
+        this.isLoggedIn$ = this.store.pipe(select(isLoggedInSelector));
+    }
 }
--- src/app/shared/modules/toolbar/toolbar.module.ts
+++ src/app/shared/modules/toolbar/toolbar.module.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { MaterialModule } from '../material/material.module';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 
 @NgModule({
     declarations: [ToolbarComponent],
-    imports: [CommonModule, MaterialModule],
+    imports: [CommonModule, MaterialModule, RouterModule],
     exports: [ToolbarComponent],
 })
 export class ToolbarModule {}
