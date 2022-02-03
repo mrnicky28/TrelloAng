5b271b594825a93bdad58455003e7c7e36e6cb6b
mrnicky28 <lavrinovich_nikita@mail.ru>
2022-02-03 11:13:47 +0300

    add todo actions types

---
+++ src/app/shared/modules/todo-list/store/todoActionsTypes.ts
@@ -0,0 +1,17 @@
+export enum TodoActionTypes {
+    ADD_TODO = '[ADD TODO] Load Add Todos',
+    ADD_TODO_SUCCESS = '[ADD TODO] Load Add Todos Success',
+    ADD_TODO_FAILURE = '[ADD TODO] Load Add Todos Failure',
+
+    DELETE_TODO = '[DELETE TODO] Load Delete Todos',
+    DELETE_TODO_SUCCESS = '[DELETE TODO] Load Delete Todos Success',
+    DELETE_TODO_FAILURE = '[DELETE TODO] Load Delete Todos Failure',
+
+    GET_TODO = '[GET TODO] Load Get Todos',
+    GET_TODO_SUCCESS = '[GET TODO] Load Get Todos Success',
+    GET_TODO_FAILURE = '[GET TODO] Load Get Todos Failure',
+
+    UPDATE_TODO = '[UPDATE TODO] Load Update Todos',
+    UPDATE_TODO_SUCCESS = '[UPDATE TODO] Load Update Todos Success',
+    UPDATE_TODO_FAILURE = '[UPDATE TODO] Load Update Todos Failure',
+}
