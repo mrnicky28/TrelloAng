7a3866b38bedc44fdf5f51fa5e42642341940cf8
mrnicky28 <lavrinovich_nikita@mail.ru>
2022-02-03 11:18:50 +0300

    update dashboard component

--- src/app/dasboard/components/dashboard/dashboard.component.ts
+++ src/app/dasboard/components/dashboard/dashboard.component.ts
@@ -1,8 +1,8 @@
 import {
     AddTodoDialogComponent
 } from 'src/app/shared/modules/add-todo-dialog/components/add-todo-dialog/add-todo-dialog.component';
+import { loadGetTodosAction } from 'src/app/shared/modules/todo-list/store/actions/todo.action';
 
-import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { Store } from '@ngrx/store';
@@ -16,32 +16,6 @@ export class DashboardComponent implements OnInit {
     constructor(private store: Store, private dialog: MatDialog) {}
 
     ngOnInit(): void {}
-    todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
-
-    done = [
-        'Get up',
-        'Brush teeth',
-        'Take a shower',
-        'Check e-mail',
-        'Walk dog',
-    ];
-
-    drop(event: CdkDragDrop<string[]>) {
-        if (event.previousContainer === event.container) {
-            moveItemInArray(
-                event.container.data,
-                event.previousIndex,
-                event.currentIndex,
-            );
-        } else {
-            transferArrayItem(
-                event.previousContainer.data,
-                event.container.data,
-                event.previousIndex,
-                event.currentIndex,
-            );
-        }
-    }
 
     // dropHorizontal(event: CdkDragDrop<string[]>) {
     //     moveItemInArray(
@@ -57,11 +31,11 @@ export class DashboardComponent implements OnInit {
             data: {},
         });
         dialogRef.afterClosed().subscribe((result) => {
-            // this.getTodos();
+            this.getTodos();
         });
     }
 
-    // getTodos() {
-    //     this.store.dispatch(getTasks());
-    // }
+    getTodos() {
+        this.store.dispatch(loadGetTodosAction());
+    }
 }
--- src/app/dasboard/dashboard.module.ts
+++ src/app/dasboard/dashboard.module.ts
@@ -1,12 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { AddTodoDialogModule } from '../shared/modules/add-todo-dialog/add-todo-dialog.module';
 import { MaterialModule } from '../shared/modules/material/material.module';
+import { TodoListModule } from '../shared/modules/todo-list/todo-list.module';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
     declarations: [DashboardComponent],
-    imports: [CommonModule, MaterialModule],
+    imports: [
+        CommonModule,
+        MaterialModule,
+        TodoListModule,
+        AddTodoDialogModule,
+    ],
     exports: [DashboardComponent],
 })
 export class DashboardModule {}
